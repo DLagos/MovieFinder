fddc2a72d6f3de9eda5142d678141472d3745740
DLagos <dionmattlagos@gmail.com>
2016-12-09 15:18:03 -0800

    added model and factory

---
+++ MovieFinder/app/src/main/java/team7/moviefinder/MovieFactory.java
@@ -0,0 +1,30 @@
+package team7.moviefinder;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+import team7.moviefinder.Movie;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by Dion on 12/9/16.
+ */
+
+public class MovieFactory {
+
+    public static List<Movie> parseJson(JSONObject jsonObject) throws JSONException {
+        List<Movie> movies = new ArrayList<>();
+        // Check if the JSONObject has object with key "Search"
+        if(jsonObject.has("Search")){
+            // Get JSONArray from JSONObject
+            JSONArray jsonArray = jsonObject.getJSONArray("Search");
+            for(int i = 0; i < jsonArray.length(); i++){
+                // Create new Movie object from each JSONObject in the JSONArray
+                movies.add(new Movie(jsonArray.getJSONObject(i)));
+            }
+        }
+
+        return movies;
+    }
+}
