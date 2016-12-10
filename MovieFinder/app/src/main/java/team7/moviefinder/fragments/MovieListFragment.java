e7aeaf6224977ca87fac6ee034d22d9e81ddd2f8
Prateek Gupta <pgupta2@mail.sfsu.edu>
2016-12-09 16:54:18 -0800

    Minor changes

--- MovieFinder/app/src/main/java/team7/moviefinder/fragments/MovieListFragment.java
+++ MovieFinder/app/src/main/java/team7/moviefinder/fragments/MovieListFragment.java
@@ -4,6 +4,8 @@ import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
@@ -26,5 +28,10 @@ public class MovieListFragment extends Fragment{
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         return super.onCreateView(inflater, container, savedInstanceState);
+    }
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        super.onCreateOptionsMenu(menu, inflater);
     }
 }
