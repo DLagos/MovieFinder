69400c873a4a19e96faffcf9f60c9283cb50ff88
Prateek Gupta <pgupta2@mail.sfsu.edu>
2016-12-09 16:16:25 -0800

    Extended classes and fragments

--- MovieFinder/app/src/main/java/team7/moviefinder/activities/SingleMovieView.java
+++ MovieFinder/app/src/main/java/team7/moviefinder/activities/SingleMovieView.java
@@ -1,8 +1,17 @@
 package team7.moviefinder.activities;
 
+import android.support.v4.app.Fragment;
+
+import team7.moviefinder.fragments.SingleFragmentActivity;
+import team7.moviefinder.fragments.SingleMovieFragment;
+
 /**
  * Created by Dion on 12/9/16.
  */
 
-public class SingleMovieView {
+public class SingleMovieView extends SingleFragmentActivity {
+    @Override
+    protected Fragment createFragment() {
+        return new SingleMovieFragment();
+    }
 }
