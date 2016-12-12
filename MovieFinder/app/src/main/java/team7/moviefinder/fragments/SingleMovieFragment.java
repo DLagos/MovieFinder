c4919906ece9d8d193106f7c605ca01a60e453ef
Billy P. Witt <billy.coco6@gmail.com>
2016-12-11 22:43:02 -0800

    SingleView

--- MovieFinder/app/src/main/java/team7/moviefinder/fragments/SingleMovieFragment.java
+++ MovieFinder/app/src/main/java/team7/moviefinder/fragments/SingleMovieFragment.java
@@ -9,12 +9,15 @@ import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.android.volley.toolbox.ImageLoader;
 import com.android.volley.toolbox.NetworkImageView;
 
 import team7.moviefinder.R;
+import team7.moviefinder.app.App;
 import team7.moviefinder.models.Movie;
 
 import team7.moviefinder.factories.MovieFactory;
+import team7.moviefinder.volley.VolleySingleton;
 
 /**
  * Created by Dion on 12/9/16.
@@ -31,6 +34,7 @@ public class SingleMovieFragment extends Fragment {
     private TextView mRelease_date;
     private TextView mAvg;
     private NetworkImageView poster;
+    ImageLoader imageLoader = VolleySingleton.getInstance(App.getContext()).getImageLoader();
 
     public static SingleMovieFragment newInstance(int id){
         Bundle args = new Bundle();
@@ -47,14 +51,16 @@ public class SingleMovieFragment extends Fragment {
         mTitle = (TextView) v.findViewById(R.id.single_movie_title_text_view);
         //mId = (TextView) v.findViewById(R.id.single_movie_avg);
         mOverview = (TextView) v.findViewById(R.id.single_movie_description_text_view);
+        mAvg = (TextView) v.findViewById(R.id.single_movie_rating);
         mRelease_date = (TextView) v.findViewById(R.id.single_movie_date);
         poster = (NetworkImageView) v.findViewById(R.id.single_movie_poster_image_view);
+
         mTitle.setText(mMovie.getTitle());
 //        mId.setText(mMovie.getId());
         mOverview.setText(mMovie.getOverview());
         mRelease_date.setText(mMovie.getDate());
-        //mAvg.setText(String.valueOf(mMovie.getAvg()));
-        //poster.setImageBitmap(mMovie.getPosterUrl());
+        mAvg.setText("Rating: " + String.valueOf(mMovie.getAvg()));
+        poster.setImageUrl(mMovie.getPosterUrl(), imageLoader);
         return v;
     }
 
